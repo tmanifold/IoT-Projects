ce816dbd279837c8d309b3ce582227165c091f98
rasputin <tdmanifold@gmail.com>
2019-11-06 17:41:58 -0500

    Reworked directory structure to include only required files for server startup. Added placeholder display resource with functionality to toggle between display 1 or display 2 based on the value stored in the the 'display/' resource

--- proj2/coapserver.py
+++ proj2/coapserver.py
@@ -12,6 +12,7 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
 temperature_resource = BasicResource()
+display_resource = BasicResource()
 
 def get_temp():
     f = open('/sys/bus/w1/devices/28-000007550451/w1_slave')
@@ -20,34 +21,60 @@ def get_temp():
     t_pos = lines[1].find("t=")
     return float(lines[1][t_pos+2:t_pos+6]) / 100
 
-def update_tmp():
+def update_temp():
     global temperature_resource
 
     while True:
         temperature_resource.payload = str(get_temp())
         sleep(2)
+
+def display_resource_listener():
     
+    global display_resource
+    
+    last_display = 2
+
+    while True:
+        if display_resource.payload == '1' and last_display == 2:
+            print "Display 1"
+            last_display = 1
+        elif display_resource.payload == '2' and last_display == 1:
+            print "Display 2"
+            last_display = 2
+        
+        sleep(1)
 
 class CoAPServer(CoAP):
+
+
+
     def __init__(self, host, port, multicast=False):
         CoAP.__init__(self, (host, port), multicast)
 
-        global temperature_resource 
+        global temperature_resource
+        global display_resource
 
         self.add_resource('temperature/', temperature_resource)
-
+        self.add_resource('display/', display_resource)
+        
+        display_resource.payload = '1'
+        
         print "CoAP Server start on " + host + ":" + str(port)
         print self.root.dump()
 
-        t = threading.Thread(target=update_tmp, args=())
-        t.start()
+        t_temp = threading.Thread(target=update_temp, args=())
+        t_disp = threading.Thread(target=display_resource_listener, args=())
+        
+        t_temp.start()
+        t_disp.start()
+    
 
 if __name__ == "__main__":
-	server = CoAPServer("0.0.0.0", 5683, False)
+    server = CoAPServer("0.0.0.0", 5683, False)
     
-	try:
-		server.listen(10)
-	except KeyboardInterrupt:
-	        print "Server Shutdown"
-	        server.close()
-	        print "Exiting..."
+    try:
+        server.listen(10)
+    except KeyboardInterrupt:
+        print "Shutting down..."
+        server.close()
+        print "Exiting..."
