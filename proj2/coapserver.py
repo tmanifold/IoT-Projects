75fbdf8f189e66ee2883295c48d9a0df687ba558
rasputin <tdmanifold@gmail.com>
2019-11-06 20:50:11 -0500

    Reworked project to use new temp+humidity sensor, and pots with LCD

--- proj2/coapserver.py
+++ proj2/coapserver.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
-
-#import sys
+import sys
 import os
 import threading
+import Adafruit_DHT
 import RPi.GPIO as GPIO
+from RPLCD.gpio import CharLCD
 from time import sleep
 from coapthon.server.coap import CoAP
 from exampleresources import BasicResource
@@ -21,12 +22,18 @@ class CoAPServer(CoAP):
     # use PUT to update 'display/' resource via Copper
     temperature_resource = BasicResource()
     display_resource = BasicResource()
+    humidity_resource = BasicResource()
+    
+    lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23], numbering_mode=GPIO.BOARD)
+    
+    lock = threading.Lock()
 
     def __init__(self, host, port, multicast=False):
         CoAP.__init__(self, (host, port), multicast)
 
         self.add_resource('temperature/', self.temperature_resource)
         self.add_resource('display/', self.display_resource)
+        self.add_resource('humidity/', self.humidity_resource)
         
         # use display 1 as default
         self.display_resource.payload = '1'
@@ -34,44 +41,44 @@ class CoAPServer(CoAP):
         print "CoAP Server start on " + host + ":" + str(port)
         print self.root.dump()
 
-        t_temp = threading.Thread(target=self.update_temp, args=())
-        t_disp = threading.Thread(target=self.display_resource_listener, args=())
+        self.lcd.clear()
+    
+        t_stats = threading.Thread(target=self.get_temp_humidity, args=())
+        t_lcd = threading.Thread(target=self.display_resource_listener, args=())
+        t_stats.start()
+        t_lcd.start()
         
-        t_temp.start()
-        t_disp.start()
-
-    # parse the temperature sensor device file
-    # returns the temperaturein celsius as a float
-    def get_temp(self):
-        f = open('/sys/bus/w1/devices/28-000007550451/w1_slave')
-        lines = f.readlines()
-        f.close()
-        t_pos = lines[1].find("t=")
-        return float(lines[1][t_pos+2:t_pos+6]) / 100
-
-    # read the temperature sensor using get_temp() and update the 'temperature/'
-    #  resource payload accordingly
-    def update_temp(self):
-
+    def get_temp_humidity(self):
+        
         while True:
-            self.temperature_resource.payload = str(self.get_temp())
-            sleep(2)
+            
+            h, t = Adafruit_DHT.read_retry(11, 4)
+            
+            self.humidity_resource.payload = str(h)
+            self.temperature_resource.payload = str(t)
+            
+            #self.lcd.cursor_pos = (0,0)
+            #self.lcd.write_string("T: %d C " % t)
+            #self.lcd.cursor_pos = (1,0)
+            #self.lcd.write_string("H: %d %%" % h)
+            
+            sleep (1)
+        
 
     # runs on a thread and periodically checks the value on 'display/' resource
     # sets the corresponding display based on the payload value
     def display_resource_listener(self):
-        
-        last_display = 2 # init to 2 so toggle flag works
-
+ 
         while True:
+            self.lcd.clear()
+            self.lcd.cursor_pos = (0,0)
+        
             # toggle to display 1
-            if self.display_resource.payload == '1' and last_display == 2:
-                print "Display 1"
-                last_display = 1
+            if self.display_resource.payload == '1':
+                self.lcd.write_string("T: %s C " % self.temperature_resource.payload)
             # toggle to display 2
-            elif self.display_resource.payload == '2' and last_display == 1:
-                print "Display 2"
-                last_display = 2
+            elif self.display_resource.payload == '2':
+                self.lcd.write_string("H: %s %%" % self.humidity_resource.payload)
             
             sleep(1)
 
