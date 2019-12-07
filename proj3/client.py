45c2a896b8a97273961fde7e69b06f6e51aa3913
Tyler Manifold <tdmanifold@gmail.com>
2019-12-07 00:07:34 -0500

    streaming video to vlc

--- proj3/client.py
+++ proj3/client.py
@@ -1,5 +1,5 @@
 
-import sys, socket
+import sys, socket, subprocess
 # import cv2
 import coapthon
 from coapthon.client.helperclient import HelperClient
@@ -13,13 +13,12 @@ TCP_PORT  = 44444
 
 try:
     IP = input ("Enter server hostname/IP: ")
-    #res = input ('Enter requested resource: ')
-    #res_path = '/' + res
-    #print ('requesting ', res_path)
+    res = input ('Enter requested resource: ')
+    res_path = ('/' + res).strip(' ')
+    print ('requesting ', res_path)
     # ask CoAP server for camera resource
     client = HelperClient(server=(IP, COAP_PORT))
-    #method = input ('1. GET\n2. OBSERVE\n> ')
-    response = client.get('snapshot')
+    response = client.get(res_path)
 
     print(response.pretty_print())
 
@@ -33,18 +32,33 @@ try:
             sock.connect((server_ip, server_port))
             print ('connected to %s:%d' % (server_ip, server_port))
 
-            with open('recv.png', 'wb') as img:
-                r = 0
-                while True:
-                    data = sock.recv(4096)
-                    if not data:
-                        break
-                    r += len(data)
-                    sys.stdout.write('received %d B         \r' % (r))
-                    sys.stdout.flush()
-                    img.write(data)
-                sys.stdout.write('\n')
+            if res == 'snapshot':
+                with open('recv.png', 'wb') as img:
+                    r = 0
+                    while True:
+                        data = sock.recv(4096)
+                        if not data:
+                            break
+                        r += len(data)
+                        sys.stdout.write('received %d B         \r' % (r))
+                        sys.stdout.flush()
+                        img.write(data)
+                    sys.stdout.write('\n')
 
+            elif res == 'video':
+                sock = sock.makefile('rb')
+                cmd = ['C:\\Program Files\\VideoLAN\\VLC\\vlc.exe', '--demux', 'h264', '-']
+                player = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+
+                try:
+                    while True:
+                        data = sock.read(1024)
+                        if not data:
+                            break
+                        player.stdin.write(data)
+                except BrokenPipeError:
+                    print('video stream terminated')
+                player.terminate()
         client.stop()
         print ('socket closed')
 
